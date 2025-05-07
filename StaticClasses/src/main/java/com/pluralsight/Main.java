9583d687209eeed8c1049c2791338a5f34eb4f18
Jiward H Torres <jiwardtorres@yahoo.com>
2025-05-07 15:14:57 -0700

    new

--- StaticClasses/src/main/java/com/pluralsight/Main.java
+++ StaticClasses/src/main/java/com/pluralsight/Main.java
@@ -5,6 +5,6 @@ public class Main {
 
         System.out.println(NameFormat.format("Jiward","Torres"));
         System.out.println(NameFormat.format("Dr", "Maya", "Milan", "Moralez", "PHD"));
-
+        System.out.println(NameFormat.format("Jiward Hingpit Torres"));
     }
 }
